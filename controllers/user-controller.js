1427457f35f741f484a07235288e4c0b589da76f
James-Fraser1 <jdfraser2022@gmail.com>
2022-09-30 22:04:02 -0400

    request to find user by specific ID created

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,12 +5,32 @@ const userController = {
     getAllUsers(req, res) {
         Thought.find({})
             .select('-__v')
-            .then(dbThoughtData => res.json(dbThoughtData))
+            .then(dbUserData => res.json(dbUserData))
             .catch(err => {
                 res.status(400).json(err);
             });
     },
 
+    getUserByID({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate([
+            { path: 'thoughts', 
+            select: '-__v'},
+            { path: 'friends', 
+            select: '-__v'},
+        ])
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No User was found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    }
 }
 
 module.exports = userController;
