87070a13b0dd1e3808b67c383123236cb489b544
James-Fraser1 <jdfraser2022@gmail.com>
2022-09-30 20:29:14 -0400

    GET request for retrieving thoughts by ID created

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,6 +1,8 @@
 const { Thought, User } = require('../models');
 
 const thoughtController = {
+    // Get Request for All Thoughts
+    // Expected sourcing -> /api/thoughts
     getAllThoughts(req, res) {
         Thought.find({})
             .populate({
@@ -11,7 +13,28 @@ const thoughtController = {
             .sort({ _id: -1 })
             .then(dbThoughtData => res.json(dbThoughtData))
             .catch(err => {
-                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // Get Request for individual Thoughts 
+    // Expected sourcing -> /api/thoughts/:id
+    getThoughtByID({ params }, res) {
+        Thought.findOne({ _id: params.id })
+            .populate({
+                path: 'user',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No Thought was found with this id' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => {
                 res.status(400).json(err);
             });
     }
