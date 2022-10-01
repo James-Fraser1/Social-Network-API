12e9557f2fd5a29f2e7584f63989dea5790a09b6
James-Fraser1 <jdfraser2022@gmail.com>
2022-09-30 21:40:31 -0400

    Thought update route created

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -44,5 +44,16 @@ const thoughtController = {
         .catch(err => {
             res.status(400).json(err);
         })
-    }
+    },
+    updateThought({ params, body }, res) {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No Thought found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
 }
